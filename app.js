53834dd770998d902ac25eeee7a05548e63b1428
Skaidrius <skaidrius@gmail.com>
2015-03-18 08:56:47 +0000

    added test controller / got rid of scope in it

--- app.js
+++ app.js
@@ -78,10 +78,11 @@ app.controller('DataControllerEn', function($scope, $interval, $filter) {
         $scope.currAP = $filter('date')($scope.input, 'a');
 
         $scope.hoursArray= [ {value:   1,     name: "One" }, 
-                        {value:   2,     name: "Two"        }, 
-                        {value:   3,     name: "Three"       }, 
-                        {value:   4,     name: "Four"   }, 
-                        {value:   6,     name: "Six"     }, 
+                        {value:   2,     name: "Two"    }, 
+                        {value:   3,     name: "Three"  }, 
+                        {value:   4,     name: "Four"   },
+                        {value:   5,     name: "Five"   },
+                        {value:   6,     name: "Six"    }, 
                         {value:   7,     name: "Seven"  }, 
                         {value:   8,     name: "Eight"  },
                         {value:   9,     name: "Nine"   },
@@ -121,11 +122,10 @@ app.controller('DataControllerEn', function($scope, $interval, $filter) {
 });
 
 // tests
-app.controller('TestController', function($scope, $interval) {  //clone for testing
+app.controller('TestController', function() {  //clone for testing
 
-    $scope.init = $interval(function(){
-
-        $scope.hoursPastHalf= [ {value:   1,     name: "Pirma" }, 
+        var vm = this;
+        vm.hoursPastHalf= [ {value:   1,     name: "Pirma" }, 
                         {value:   2,     name: "Dvi"        }, 
                         {value:   3,     name: "Trys"       }, 
                         {value:   4,     name: "Keturios"   }, 
@@ -137,10 +137,10 @@ app.controller('TestController', function($scope, $interval) {  //clone for test
                         {value:  12,     name: "Dvylika"    },
                         {value:  13,     name: "Pirma"      } ];
         
-        $scope.tenHours = {value:  10,  name: "Dešimt"      };
-        $scope.five =   {value:  5,     name:  "Penkios"    };
+        vm.tenHours = {value:  10,  name: "Dešimt"      };
+        vm.five =   {value:  5,     name:  "Penkios"    };
                         
-        $scope.hoursPastZero = [ {value:  1,     name: "Pirmos"     }, 
+        vm.hoursPastZero = [ {value:  1,     name: "Pirmos"     }, 
                         {value:  2,     name: "Dviejų"      }, 
                         {value:  3,     name: "Trijų"       }, 
                         {value:  4,     name: "Keturių"     }, 
@@ -153,36 +153,43 @@ app.controller('TestController', function($scope, $interval) {  //clone for test
                         {value:  12,    name: "Dvylikos"    },
                         {value:  13,    name: "Pirmos"      }  ];  
 
-        $scope.till = "Be";
-        $scope.after = "Po";
+        vm.till = "Be";
+        vm.after = "Po";
         
-        $scope.fivetill = "Penkių";
-        $scope.ten = "Dešimt";
-        $scope.quarter = "Penkiolikos";
-        $scope.fifteen = "Penkiolika";
-        $scope.twenty = "Dvidešimt";
-        $scope.half = "Pusė";
-        $scope.afterhalf = "Pusės";
+        vm.fivetill = "Penkių";
+        vm.ten = "Dešimt";
+        vm.quarter = "Penkiolikos";
+        vm.fifteen = "Penkiolika";
+        vm.twenty = "Dvidešimt";
+        vm.half = "Pusė";
+        vm.afterhalf = "Pusės";
     
-        $scope.hour = "Valanda";
-        $scope.hours = "Valandos";
-        $scope.morehour = "Valandų";
+        vm.hour = "Valanda";
+        vm.hours = "Valandos";
+        vm.morehour = "Valandų";
         
-        $scope.showClassName = 'active';
-        $scope.hiddenClassName = 'passive';
+    vm.showClassName = 'active';
+    vm.hiddenClassName = 'passive';
 
-},100 );
-
-});
-
-app.directive('showBigClock', function(){
-    return {
-        templateUrl: 'clock.html'
-    };
 });
 
 app.directive('showBigClockEn', function(){
     return {
+        restrict: 'E',
         templateUrl: 'clockEn.html'
+    };
+});
+
+app.directive('showBigClock', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'clock.html'
+    };
+});
+
+app.directive('showTestClock', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'clocktest.html'
     };
 });
